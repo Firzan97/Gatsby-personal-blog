112267a45a35434c23e880858f862ef43438fcf0
Firzan97 <FirzanAzrai97@gmail.com>
2020-08-02 02:31:55 +0800

    Update APIURL for strapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,9 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: process.env.DEPLOY_URL
+          ? "https://tranquil-bayou-12167.herokuapp.com"
+          : "http://localhost:1337",
         queryLimit: 1000, // Default to 100
         //   contentTypes : `jobs`, `projects`, `blogs`,
         //   singleType : `about`
